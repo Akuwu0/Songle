78480efe98bb2a83c9203ab28fff5dcd9dee8426
Adrián Martín Muñoz <adrianmartinm04@gmail.com>
2025-01-02 22:10:55 +0100

    edit button for index.html and delete the event for button in script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-let btn_comenzar = document.getElementById('btn_comenzar');
-
-const comenzar = () => {
-    location.href = "./pages/juego.html";
-}
-
-btn_comenzar.addEventListener('click', comenzar);
