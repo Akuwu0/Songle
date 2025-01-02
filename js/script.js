b2fcaa4696070c10db06fb844b1e15f1928875d0
Adrián Martín Muñoz <adrianmartinm04@gmail.com>
2025-01-02 13:46:08 +0100

    The structure of the protector is created and the game start page and the event are necessary in the index

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let btn_comenzar = document.getElementById('btn_comenzar');
+
+const comenzar = () => {
+    location.href = "./pages/juego.html";
+}
+
+btn_comenzar.addEventListener('click', comenzar);
